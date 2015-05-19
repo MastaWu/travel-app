a6e03a636f47453ea1c86b9ccbdfc50794f856d2
MastaWu <wu.stephend@gmail.com>
2015-05-19 18:40:56 -0400

    added route

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+angular.module('travel-app', [
+
+    'ngAnimate',
+    'ui.router',
+    'uiGmapgoogle-maps',
+    'ui.bootstrap'
+
+]).
+
+    config(function($stateProvider, $urlRouteProvider){
+       $urlRouteProvider.otherwise('/home');
+
+        $stateProvider.
+            state('/home',{
+                url:'/home',
+                views: {
+                    '':{
+                        templateUrl: 'views/main.html'
+                    }
+                }
+            }
+            )
+    });
